a0e7643c26a8c34c07b3f175179ed0ce761ae7cb
Johan Chrillesen <johan.chrillesen@epitech.eu>
2024-02-01 17:12:02 -0500

    Add IDEWorkspaceChecks.plist and update build:ios script

--- src/common/queries/weatherData.ts
+++ src/common/queries/weatherData.ts
@@ -21,29 +21,22 @@ export type CurrentWeatherData = {
     last_updated_epoch: number
     last_updated: string
     temp_c: number
-    temp_f: number
     is_day: number
     condition: {
       text: string
       icon: string
       code: number
     }
-    wind_mph: number
     wind_kph: number
     wind_degree: number
     wind_dir: string
     pressure_mb: number
-    pressure_in: number
     precip_mm: number
-    precip_in: number
     humidity: number
     cloud: number
     feelslike_c: number
-    feelslike_f: number
     vis_km: number
-    vis_miles: number
     uv: number
-    gust_mph: number
     gust_kph: number
   }
 }
@@ -52,39 +45,29 @@ type HourlyWeatherData = {
   time_epoch: number
   time: string
   temp_c: number
-  temp_f: number
   is_day: number
   condition: {
     text: string
     icon: string
     code: number
   }
-  wind_mph: number
   wind_kph: number
   wind_degree: number
   wind_dir: string
   pressure_mb: number
-  pressure_in: number
   precip_mm: number
-  precip_in: number
   snow_cm: number
   humidity: number
   cloud: number
   feelslike_c: number
-  feelslike_f: number
   windchill_c: number
-  windchill_f: number
   heatindex_c: number
-  heatindex_f: number
   dewpoint_c: number
-  dewpoint_f: number
   will_it_rain: number
   chance_of_rain: number
   will_it_snow: number
   chance_of_snow: number
   vis_km: number
-  vis_miles: number
-  gust_mph: number
   gust_kph: number
   uv: number
   short_rad: number
@@ -96,18 +79,12 @@ export type DailyWeatherData = {
   date_epoch: number
   day: {
     maxtemp_c: number
-    maxtemp_f: number
     mintemp_c: number
-    mintemp_f: number
     avgtemp_c: number
-    avgtemp_f: number
     maxwind_mph: number
-    maxwind_kph: number
     totalprecip_mm: number
-    totalprecip_in: number
     totalsnow_cm: number
     avgvis_km: number
-    avgvis_miles: number
     avghumidity: number
     daily_will_it_rain: number
     daily_chance_of_rain: number
@@ -145,22 +122,22 @@ export type FullWeatherWeekData = {
 async function getWeather(cityName: string): Promise<FullWeatherWeekData> {
   // eslint-disable-next-line no-useless-catch
   try {
-    const weatherResponse = await fetch(
-      `https://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=${cityName}&aqi=yes&lang=fr`
-    )
-
-    const weatherData = await weatherResponse.json()
-
     const weatherWeekResponse = await fetch(
       `https://api.weatherapi.com/v1/forecast.json?key=${WEATHER_API_KEY}&q=${cityName}&days=7&lang=fr`
     )
 
     const weatherWeekData = await weatherWeekResponse.json()
 
+    if (weatherWeekData.error) {
+      throw new Error(weatherWeekData.error.message)
+    }
+
+    // if (weatherWeekData.)
+
     return {
-      current: weatherData.current,
+      current: weatherWeekData.current,
       forecast: weatherWeekData.forecast,
-      location: weatherData.location,
+      location: weatherWeekData.location,
     }
   } catch (err) {
     throw err
